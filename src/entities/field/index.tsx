180757a200694d7afe5827fde1cd6f456a249cda
Alexandr Yanykin <numelen@gmail.com>
2022-10-20 00:07:53 +0300

    feat: added Field component

--- src/entities/field/index.tsx
+++ src/entities/field/index.tsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
-import { useState } from 'react';
-import { Christmas } from '@shared/ui';
+import { useState, useEffect } from 'react';
+import { Christmas, IconProps } from '@shared/ui';
 import { Cell } from '@shared/ui';
 import styles from './styles.module.scss';
 
@@ -10,15 +10,41 @@ interface FieldProps {
 
 export const Field = ({ size }: FieldProps) => {
   const [active, setActive] = useState(false);
+  const [elements, setElements] = useState<
+    (({ size, ...props }: IconProps) => JSX.Element)[]
+  >([]);
+
+  useEffect(() => {
+    const values = Object.values(Christmas);
+    const arrayField: (({ size, ...props }: IconProps) => JSX.Element)[] =
+      Array(size);
+    const arrayOfIndexes = Array(size)
+      .fill(null)
+      .map((_, i) => i);
+
+    for (let i = 0; i < size; i++) {
+      const randomIndex = Math.floor(Math.random() * arrayOfIndexes.length);
+      arrayField[arrayOfIndexes[randomIndex]] = values[Math.floor(i / 2)];
+
+      arrayOfIndexes.splice(randomIndex, 1);
+    }
+
+    setElements(arrayField);
+  }, [size]);
 
   return (
     <div className={classNames(styles.field, styles[`size${size}`])}>
-      <Cell
-        onClick={() => setActive((prev) => !prev)}
-        cover={<Christmas.House />}
-        icon={<Christmas.Bird />}
-        active={active}
-      />
+      {elements.map((item) => {
+        const Icon = item;
+        return (
+          <Cell
+            onClick={() => setActive((prev) => !prev)}
+            cover={<Christmas.House />}
+            icon={<Icon />}
+            active={active}
+          />
+        );
+      })}
     </div>
   );
 };
--- src/shared/ui/icons/index.ts
+++ src/shared/ui/icons/index.ts
@@ -1,1 +1,2 @@
 export * from './christmas';
+export * from './icon';
