d075c1cfeebea66fb611c8fe4728ab88b12e2f9c
Alexandr Yanykin <al.yanykin@gmail.com>
2023-12-25 23:07:51 +0300

    feat: added info-card tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,31 @@
-describe('My First Test', () => {
-  it('Посетить главную страницу', () => {
+describe('main page', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:5173/');
+  });
+
+  it('Information cards should exist on the main page.', () => {
+    cy.get('[data-cy="info-card"]').each((card, index) => {
+      if (index === 0) {
+        cy.wrap(card)
+          .find('[data-cy="info-card-progress-bar"]')
+          .should('exist');
+
+        cy.wrap(card)
+          .find('[data-cy="info-card-title"]')
+          .should('exist')
+          .contains('Pairs matched');
+
+        cy.wrap(card).find('[data-cy="info-card-info"]').should('exist');
+      }
+
+      if (index === 1) {
+        cy.wrap(card)
+          .find('[data-cy="info-card-title"]')
+          .should('exist')
+          .contains('Total moves');
+
+        cy.wrap(card).find('[data-cy="info-card-info"]').should('exist');
+      }
+    });
   });
 });
